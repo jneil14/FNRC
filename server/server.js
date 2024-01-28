48c256efb0b6164cbaa19ff19f6318ff1d0b3a48
Neil Solon <neilsolon@Neils-MacBook-Pro.local>
2024-01-27 22:24:23 -0800

    updated dependencies and react&node versions

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.post("/api/form", (req, res) => {
+  console.log(req.body);
 
-app.post('/api/form', (req, res) => {
-    console.log(req.body);
-
-    let data = req.body;
-    const htmlEmail = `
+  let data = req.body;
+  const htmlEmail = `
          <h3>Information</h3>
             <ul>
                 <li>Name: ${data.name}</li>
@@ -23,41 +22,35 @@ app.post('/api/form', (req, res) => {
             <p>${data.message}</p>
     `;
 
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    port: 465,
+    auth: {
+      user: "footprintsnursing@gmail.com",
+      pass: "marilee18"
+    }
+  });
 
-    const transporter = nodemailer.createTransport({
-        service:'Gmail',
-        port: 465,
-        auth:{
-            user: 'footprintsnursing@gmail.com',
-            pass: 'marilee18'
-      }
-    });
+  let mailOptions = {
+    from: data.email,
+    to: "footprintsnursing@gmail.com",
+    replyTo: data.email,
+    subject: `Message from ${data.name}`,
+    html: htmlEmail
+  };
 
-
-    let mailOptions = {
-      from: data.email,
-      to: 'footprintsnursing@gmail.com',
-      replyTo: data.email,
-      subject: `Message from ${data.name}`,
-      html: htmlEmail
-    };
-
-
-    transporter.sendMail(mailOptions, (err, info) => {
-        
-        if(err) {
-            return console.log(err)
-        }
-        else {
-            console.log('Message sent: %s', info.message )
-            console.log('Message URL: %s', nodemailer.getTestMessageUrl(info))
-        }
-    })
-
-})
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log("Message sent: %s", info.message);
+      console.log("Message URL: %s", nodemailer.getTestMessageUrl(info));
+    }
+  });
+});
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-})
+  console.log(`Server listening on port ${PORT}`);
+});
